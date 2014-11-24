fb1d5c2f76660b96136231818d7b2b86aa066e86
fish_ball <alfred.h@163.com>
2014-11-24 20:07:32 +0800

    v0.1.1. update README

--- README.md
+++ README.md
@@ -1,9 +1,46 @@
 phpexcel-api
 ============
 
-用于创建一个 php 处理 excel 的 http api，供其他程序语言调用。
+一个 php 处理 excel 的 http api，供其他程序语言调用。
 
-简单的想法：
+基本架构：
+---------
 
-Post 数据包括上传（Post 请求体）一个模板的 excel 文件，然后输入一个坐标映射到数据的键值对。
-然后将数据填入到上传的 excel 模板，并且启动 http 下载。
+通过 HTTP 提交一个 Post请求，然后返回一个下载 Excel 的响应。
+
+可以通过一个如下的表单来模拟这个请求：
+
+```http
+<form action="xlsapi/index.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="template" />
+    <textarea name="data"></textarea>
+    <input type="submit" />
+</form>
+```
+
+请求方式是 multipart/form-data，同时提交表单数据以及附件。
+
+因此，请求头应当符合如下格式，这一点请自行参见 multipart/form-data 请求的实现方法：
+[Request Headers]
+**Content-Type:** Content-Type:multipart/form-data; boundary=BOUNDER_STRING
+
+因此，POST 的请求体里面包括两个主要的部分：
+
+1. $_FILE['template']: 用于作为模板的 excel 文件，然后通过。（缺省为空 xls 文件）
+2. $_POST['data']: 用于传递填充模板的“脚本”，具体语法后面会进行叙述。
+
+另外，配置参数通过 $_POST['config'] 提供 json 的配置字典，用于覆盖 config 的默认值。
+具体配置参数后面会提到。
+
+请求发出之后，本接口会打开模板 xls 文件，然后顺着脚本内容填充数据，最后返回一个响应下载 xls。
+
+具体的响应头如下：
+
+[Response Headers]
+**Content-Disposition:** attachment; filename="excel.xls"
+**Content-Type:** application/vnd.ms-excel
+
+注意两点：
+1. 如果上传的模板文件是 xlsx 的 2007 格式，响应回来的格式会是 xlsx，否则为 xls；
+2. 返回的文件名是配置的文件名，缺省为模板的文件名，如模板缺省，则为 excel.xls。
+
--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php include 'phpexcel/Classes/PHPExcel.php';
 
+//print_r($_FILES);
+//exit();
+
 define('MIME_XLS', 'application/vnd.ms-excel');
 define('MIME_XLSX', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
 
@@ -7,8 +10,21 @@ define('MIME_XLSX', 'application/vnd.openxmlformats-officedocument.spreadsheetml
 $export_name = 'excel.xls';
 $export_type = 'Excel5';
 $export_mime = MIME_XLS;
-$objPHPExcel = new PHPExcel();
 
+$default_config = array(
+    'export_name' => 'excel.xls',
+    'export_type' => 'Excel5', // can be Excel2007
+    'export_mime' => MIME_XLS, // can be MIME_XLSX
+    'row_delimeter' => "\r",
+    'col_delimeter' => "\t",
+
+);
+
+$config = isset($_POST['config']) ? json_decode($_POST['config']) : array();
+$config = array_merge($default_config);
+
+
+$objPHPExcel = new PHPExcel();
 if(!empty($_FILES['template']) && $_FILES['template']['error'] === 0) {
 
     $file_uploaded = $_FILES['template'];
@@ -30,16 +46,28 @@ if(!empty($_FILES['template']) && $_FILES['template']['error'] === 0) {
 // Fill
 $worksheet = $objPHPExcel->setActiveSheetIndex(0);
 if(!empty($_POST['data'])) {
-    foreach(explode(chr(13), $_POST['data']) as $row) {
-        $args = explode(chr(9), trim($row));
+    foreach(explode($config['row_delimeter'], $_POST['data']) as $row) {
+        $args = explode($config['col_delimeter'], trim($row));
         $worksheet->setCellValue($args[0], $args[1]);
     }
 }
 
+
 // Export
 // Redirect output to a client's web browser
+$ua = $_SERVER["HTTP_USER_AGENT"];
+$export_name_encoded = str_replace("+", "%20",urlencode($export_name));
+if (preg_match("/MSIE/", $ua)) {
+    header('Content-Disposition: attachment; filename="' . $export_name_encoded . '"');
+} else if (preg_match("/Firefox/", $ua)) {
+    header('Content-Disposition: attachment; filename*="utf8\'\'' . $export_name_encoded . '"');
+} else if (preg_match("/python/i", $ua)) {
+    header('Content-Disposition: attachment; filename="' . $export_name_encoded . '"');
+} else {
+    header('Content-Disposition: attachment; filename="' . $export_name . '"');
+}
+//header("Content-Disposition: attachment;filename=\"".urlencode($export_name)."\"");
 header("Content-Type: $export_mime");
-header("Content-Disposition: attachment;filename=\"$export_name\"");
 header("Cache-Control: max-age=0");
 
 $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, $export_type);
